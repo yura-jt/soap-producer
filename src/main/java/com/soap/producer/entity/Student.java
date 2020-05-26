7aa29a8e592a5b7aa93274feed6bf9922105881e
yura_jt <cryobyte.zero@gmail.com>
2020-05-26 12:42:51 +0300

    fix hibernate sequence issue

--- src/main/java/com/soap/producer/entity/Student.java
+++ src/main/java/com/soap/producer/entity/Student.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
@@ -17,7 +18,7 @@ import javax.validation.constraints.NotNull;
 @Table(name = "students")
 public class Student {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
