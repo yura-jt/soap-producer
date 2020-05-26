274677d01f9f8cfbb182593d116e866e51153f55
yura_jt <cryobyte.zero@gmail.com>
2020-05-26 09:08:24 +0300

    add soap configuration and endpoint

---
+++ src/main/java/com/soap/producer/config/WebServiceConfig.java
@@ -0,0 +1,37 @@
+package com.soap.producer.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+public class WebServiceConfig extends WsConfigurerAdapter {
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+    @Bean(name = "students")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("StudentsPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://soap.producer");
+        wsdl11Definition.setSchema(countriesSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema countriesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("students.xsd"));
+    }
+
+}
--- src/main/java/com/soap/producer/mapper/StudentDtoMapper.java
+++ src/main/java/com/soap/producer/mapper/StudentDtoMapper.java
@@ -3,6 +3,7 @@ package com.soap.producer.mapper;
 import com.soap.producer.dto.StudentDto;
 import com.soap.producer.entity.Student;
 import org.mapstruct.Mapper;
+import producer.soap.StudentSoapDto;
 
 @Mapper(componentModel = "spring")
 public interface StudentDtoMapper {
@@ -10,5 +11,9 @@ public interface StudentDtoMapper {
     StudentDto toStudentDto(Student student);
 
     Student toStudent(StudentDto studentDto);
+
+    StudentSoapDto toStudentSoapDto(Student student);
+
+    StudentDto toStudentDtoFromSoapDto(StudentSoapDto studentSoapDto);
 
 }
